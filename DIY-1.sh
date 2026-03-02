950cd0be7b20184bdc9732b50eb9e140a91e9328
time2049 <58364786+time2049@users.noreply.github.com>
2026-03-02 15:17:50 +0800

    Update DIY-1.sh

--- DIY-1.sh
+++ DIY-1.sh
@@ -2,7 +2,10 @@
 # =======================================================================================================================
 # 1-添加 opentomcat 主题 (如果你还需要的话)
 git clone https://github.com/WukongMaster/luci-theme-opentomcat.git package/luci-theme-opentomcat
-
-# 2-添加 PassWall 插件
+# 4-添加 OpenClash 插件
+sed -i '$a\src-git openclash https://github.com/vernesong/OpenClash' ./feeds.conf.default
+# 3-添加 PassWall 插件
 echo "src-git passwall_luci https://github.com/Openwrt-Passwall/openwrt-passwall.git;main" >> "feeds.conf.default"
 echo "src-git passwall_packages https://github.com/Openwrt-Passwall/openwrt-passwall-packages.git;main" >> "feeds.conf.default"
+# 4-添加 ShadowSocksR Plus+ 插件
+echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
