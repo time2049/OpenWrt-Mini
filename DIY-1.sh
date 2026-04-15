ce9a2bf0090a3a795806c2900329a25169cbf027
time2049 <58364786+time2049@users.noreply.github.com>
2026-04-15 17:43:07 +0800

    Update DIY-1.sh

--- DIY-1.sh
+++ DIY-1.sh
@@ -1,18 +1,17 @@
-# DIY-1 此脚本功能：添加外部插件
+#!/bin/bash
+# DIY-1 此脚本功能：添加外部插件 (针对 OpenWrt 24.10 优化版)
 # =======================================================================================================================
 
-# 1-添加 ShadowSocksR Plus+ 插件
-#echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
-
-# 2-添加 PowerOff 关机插件
+# 1. 添加 PowerOff 关机插件
+# 这个插件比较轻量，兼容性尚可
 git clone https://github.com/WukongMaster/luci-app-poweroff.git package/luci-app-poweroff
 
-# 3-添加 opentomcat 主题
-git clone https://github.com/WukongMaster/luci-theme-opentomcat.git package/luci-theme-opentomcat
+# 2. 添加 Argon 主题 (替代崩溃的 opentomcat)
+# Argon 对 ucode 架构支持最好，是目前 24.10 固件的标配
+git clone -b 18.06 https://github.com/jerrykuku/luci-theme-argon.git package/luci-theme-argon
+git clone https://github.com/jerrykuku/luci-app-argon-config.git package/luci-app-argon-config
 
-# 4-添加 OpenClash 插件
-#sed -i '$a\src-git openclash https://github.com/vernesong/OpenClash' ./feeds.conf.default
-
-# 5-添加 PassWall 插件
+# 3. 添加 PassWall 插件
+# 保持原样，这是最稳妥的 feeds 添加方式
 echo "src-git passwall_luci https://github.com/Openwrt-Passwall/openwrt-passwall.git;main" >> "feeds.conf.default"
 echo "src-git passwall_packages https://github.com/Openwrt-Passwall/openwrt-passwall-packages.git;main" >> "feeds.conf.default"
