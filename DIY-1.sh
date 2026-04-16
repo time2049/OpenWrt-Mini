ffeb5fda1a8170cbcea107b37bc6074ac5a90854
time2049 <58364786+time2049@users.noreply.github.com>
2026-04-16 19:48:00 +0800

    Update DIY-1.sh

--- DIY-1.sh
+++ DIY-1.sh
@@ -14,10 +14,3 @@ git clone https://github.com/WukongMaster/luci-app-poweroff.git package/luci-app
 # 这一步直接替换为你红圈选中的 2.3.2 源码分支
 git clone -b v2.3.2 https://github.com/jerrykuku/luci-theme-argon.git package/luci-theme-argon
 git clone https://github.com/jerrykuku/luci-app-argon-config.git package/luci-app-argon-config
-
-# 4. 添加 PassWall 插件
-# 使用 sed 确保直接写入 feeds.conf.default 末尾，比 echo 更稳健
-src-git passwall_packages https://github.com/Openwrt-Passwall/openwrt-passwall-packages.git;main
-src-git passwall_luci https://github.com/Openwrt-Passwall/openwrt-passwall.git;main
-
-# 注意：SSR+ 和 OpenClash 已移除
